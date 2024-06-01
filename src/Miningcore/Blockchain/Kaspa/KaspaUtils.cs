6b9f751fca2e44d8ee659cbceb77fa866c8d2673
andoril1 <141507396+andoril1@users.noreply.github.com>
2024-06-01 10:23:12 +0200

    Final Fix

--- src/Miningcore/Blockchain/Kaspa/KaspaUtils.cs
+++ src/Miningcore/Blockchain/Kaspa/KaspaUtils.cs
@@ -431,16 +431,7 @@ public class KaspaAddressUtility
                     { ConsensusNetworkConstants.ChainPrefixTestnet, KaspaBech32Prefix.KaspaTest },
                     { ConsensusNetworkConstants.ChainPrefixSimnet, KaspaBech32Prefix.KaspaSim },
                 };
-                break;
-            case "PUG":
-                this.stringsToBech32Prefixes = new Dictionary<string, KaspaBech32Prefix>
-                {
-                    { PugdagConstants.ChainPrefixMainnet, KaspaBech32Prefix.KaspaMain },
-                    { PugdagConstants.ChainPrefixDevnet, KaspaBech32Prefix.KaspaDev },
-                    { PugdagConstants.ChainPrefixTestnet, KaspaBech32Prefix.KaspaTest },
-                    { PugdagConstants.ChainPrefixSimnet, KaspaBech32Prefix.KaspaSim },
-                };
-                break;           
+                break;         
             case "NXL":
                 this.stringsToBech32Prefixes = new Dictionary<string, KaspaBech32Prefix>
                 {
